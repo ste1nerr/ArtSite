f1c3343e09ad8a3ffbe9c245525eb36a0aea4d10
ste1nerr <99362390+ste1nerr@users.noreply.github.com>
2022-12-14 22:46:17 +0200

    navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import HeaderCSS from './Header.module.scss'
-
 const Header = () => {
-  return (
-    <p className={HeaderCSS}>Header</p>
-  )
+    return (
+        <div>
+            <div className='container'>
+
+                <ul className={HeaderCSS.header_list}>
+                    <img className={HeaderCSS.header_logo} src="../images/logotype.png" alt="" />
+                    <nav className={HeaderCSS.navigation_menu}>
+                        <li className={HeaderCSS.item}>About</li>
+                        <li className={HeaderCSS.item}>Place for</li>
+                        <li className={HeaderCSS.item}>Works</li>
+                        <li className={HeaderCSS.item}>Questions</li>
+                    </nav>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default Header
