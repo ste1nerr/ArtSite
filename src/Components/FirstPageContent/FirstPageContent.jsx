4355fb01e0eafcf115559488e05578a62e0bb2da
ste1nerr <99362390+ste1nerr@users.noreply.github.com>
2022-12-16 16:02:39 +0200

    add styles to first page

--- src/Components/FirstPageContent/FirstPageContent.jsx
+++ src/Components/FirstPageContent/FirstPageContent.jsx
@@ -4,10 +4,15 @@ import React from 'react'
 const FirstPageContent = () => {
     return (
         <div className='container'>
-            <p>Ceramics</p>
-            <h1 className={FPC.under_title}>Your Vision of art</h1>
-            <p className={FPC.under_subtitle}>Handmade</p>
-            <button>First Class</button>
+
+
+            <div className={FPC.content_container}>
+                <p className={FPC.up_subtitle}>Ceramics</p>
+                <h1 className={FPC.under_title}>Your Vision of art</h1>
+                <p className={FPC.under_subtitle}>Handmade</p>
+                <button>First Class</button>
+            </div>
+
 
         </div>
     )
